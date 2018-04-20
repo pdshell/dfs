0a98154d8ddd83d303ac56348665d9736db5c9c1
248237765@qq.com <030786418@163.com>
2018-04-20 11:22:41 +0800

    1

--- jt-dubbo-cart/src/main/java/com/jt/cart/mapper/CartMapper.java
+++ jt-dubbo-cart/src/main/java/com/jt/cart/mapper/CartMapper.java
@@ -2,7 +2,7 @@ package com.jt.cart.mapper;
 
 import com.jt.common.mapper.SysMapper;
 import com.jt.dubbo.pojo.Cart;
-
+//·Ç³£ºÃ
 public interface CartMapper extends SysMapper<Cart>{
 
 	void updateCartNum(Cart cart);
